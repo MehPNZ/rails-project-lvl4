7197fa30b0895d63a017d945824ea5cacbc14e94
Александр Михайлов <meh@MacBook-Air-Aleksandr.local>
2022-08-26 17:24:38 +0300

    deploy

--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,11 @@ Rails.application.routes.draw do
     end
   end
 
-  scope module: :api do
+  namespace :api do
     scope module: :checks do
       post 'auth/:provider', to: 'auth#request', as: :auth_request
       get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
     end
-    post 'checks', to: 'checks#create', as: :api_checks
+    post 'checks', to: 'checks#create', as: :checks
   end
 end
