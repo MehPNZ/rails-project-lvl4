c370653a3b7922a77350f67bf0ee846209d6e47f
Александр Михайлов <meh@MacBook-Air-Aleksandr.local>
2022-08-29 14:01:10 +0300

    add test

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -2,6 +2,7 @@
 
 class Web::AuthController < Web::ApplicationController
   def callback
+    puts auth
     email = auth[:info][:email].downcase
     nickname = auth[:info][:nickname]
     token = auth[:credentials][:token]
--- app/jobs/repository_check_job.rb
+++ app/jobs/repository_check_job.rb
@@ -7,7 +7,7 @@ class RepositoryCheckJob < ApplicationJob
   queue_as :default
 
   def perform(id)
-    Open3.capture2("rm -rf #{Rails.root.join('/tmp/repos/')}")
+    Open3.capture2("rm -rf #{Rails.root}/tmp/repos/")
 
     check = RepositoryCheck.find(id)
 
@@ -18,7 +18,7 @@ class RepositoryCheckJob < ApplicationJob
     send("#{check.repository.language}_build", file, check)
 
     check.to_finish! if check.may_to_finish?
-    Open3.capture2("rm -rf #{Rails.root.join('/tmp/repos/')}")
+    Open3.capture2("rm -rf #{Rails.root}/tmp/repos/")
 
     if !check.passed || check.failed?
       UserMailer.with(check: check).check_email.deliver_now
